8d3f8f895a0f3d3185c4c095eaea28fed85f16df
skymidy <35444301+skymidy@users.noreply.github.com>
2022-10-23 23:12:09 +1000

    public class AuthInput.cs implemented

--- Objects/AuthInput.cs
+++ Objects/AuthInput.cs
@@ -1,6 +1,16 @@
 ﻿namespace ASPnetWebApp.Objects;
-
+/// <summary>
+/// User's authentication data class
+/// </summary>
 public class AuthInput
 {
-    
+    /// <summary>
+    /// User's login
+    /// </summary>
+    public string Login { get; set; }
+
+    /// <summary>
+    /// User' password
+    /// </summary>
+    public string Password { get; set; }
 }
