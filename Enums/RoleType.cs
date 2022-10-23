54718801fbd7e4bb1b32f282639fff00279d4ad3
skymidy <35444301+skymidy@users.noreply.github.com>
2022-10-23 22:55:46 +1000

    enum RoleType added static class RoleType added static method FromString added

--- Enums/RoleType.cs
+++ Enums/RoleType.cs
@@ -1,6 +1,30 @@
 ﻿namespace ASPnetWebApp.Enums;
 
-public class RoleType
+public enum RoleType
 {
-    
+    /// <summary>
+    /// Unknown user
+    /// </summary>
+    Undefined = 0,
+    /// <summary>
+    /// Administrator
+    /// </summary>
+    Admin = 1,
+    /// <summary>
+    /// Default user
+    /// </summary>
+    User = 2
+}
+public static class Role
+{
+    public static RoleType FromString(string str)
+    {
+        foreach (RoleType role in Enum.GetValues(typeof(RoleType)))
+        {
+            if (role.ToString() == str)
+                return role;
+        }
+
+        return RoleType.Undefined;
+    }
 }
