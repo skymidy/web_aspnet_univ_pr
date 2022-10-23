43bc602bc2e40077431d532728242db0a5419cdf
skymidy <35444301+skymidy@users.noreply.github.com>
2022-10-23 23:09:02 +1000

    public class AuthInfo implemented

--- Objects/AuthInfo.cs
+++ Objects/AuthInfo.cs
@@ -1,6 +1,46 @@
-﻿namespace ASPnetWebApp.Objects;
+﻿using ASPnetWebApp.Enums;
 
+namespace ASPnetWebApp.Objects;
+
+/// <summary>
+/// User's information class
+/// </summary>
 public class AuthInfo
 {
+    /// <summary>
+    /// User's authentication status
+    /// </summary>
+    public bool IsAuthenticated { get; private set; }
     
+    /// <summary>
+    /// User's role
+    /// </summary>
+    public RoleType Role { get; private set; }
+
+    /// <summary>
+    /// User's successful authentication method 
+    /// </summary>
+    /// <param name="role">User's role</param>
+    /// <returns></returns>
+    public static AuthInfo Success(RoleType role)
+    {
+        return new AuthInfo()
+        {
+            IsAuthenticated = true,
+            Role = role
+        };
+    }
+
+    /// <summary>
+    /// User's failed authentication method 
+    /// </summary>
+    /// <returns></returns>
+    public static AuthInfo Fail()
+    {
+        return new AuthInfo()
+        {
+            IsAuthenticated = false,
+            Role = RoleType.Undefined
+        };
+    }
 }
